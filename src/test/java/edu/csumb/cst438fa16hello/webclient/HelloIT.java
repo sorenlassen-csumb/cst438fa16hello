b22c6fa1d0cf3239480e6ead110d986c269db9a8
Soren Lassen <slassen@csumb.edu>
2016-09-18 19:58:59 -0700

    Added WebDriver tests
    Test plan:
    Ran "mvn integration-test" and saw the 2 webdriver tests passed.

---
+++ src/test/java/edu/csumb/cst438fa16hello/webclient/HelloIT.java
@@ -0,0 +1,59 @@
+package edu.csumb.cst438fa16hello.webclient;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+// Based on Selenium2Example from
+// http://docs.seleniumhq.org/docs/03_webdriver.jsp#introducing-the-selenium-webdriver-api-by-example
+// modified for JUnit.
+//
+// Since the class name ends in IT, it's run with "mvn integration-test", not "mvn test".
+public class HelloIT {
+    // On Mac run "brew install googledriver" to make this work.
+    WebDriver driver = new ChromeDriver();
+
+    @Before
+    public void setUp() {
+        driver.get("http://localhost:8080/hello.html");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit(); // close browser
+    }
+
+    @Test
+    public void testTitle() {
+        assertThat(driver.getTitle(), equalTo("Hello"));
+    }
+
+    @Test
+    public void testGreetsByName() {
+        // Sanity check: greeting is empty when the page is first loaded
+        assertThat(driver.findElement(By.id("greet")).getText(), equalTo(""));
+
+        // Enter something to search for
+        driver.findElement(By.id("name")).sendKeys("Alan");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        driver.findElement(By.id("submit")).click();
+
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return !d.findElement(By.id("greet")).getText().isEmpty();
+            }
+        });
+
+        assertThat(driver.findElement(By.id("greet")).getText(), equalTo("hello Alan"));
+    }
+}
