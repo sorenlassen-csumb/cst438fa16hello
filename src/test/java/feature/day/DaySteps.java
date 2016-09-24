99587f3e5330b5041127ae461553bf7495d83b27
Soren Lassen <slassen@csumb.edu>
2016-09-23 22:49:04 -0700

    Moved invocation of DayOfWeek method to When step
    Test plan: mvn test

--- src/test/java/feature/day/DaySteps.java
+++ src/test/java/feature/day/DaySteps.java
@@ -14,16 +14,15 @@ import edu.csumb.cst438fa16hello.DayOfWeek;
 import java.util.Calendar;
 
 public class DaySteps {
-    Calendar calendar;
+    String day;
 
     @When("^I request the day on (\\d+/\\d+/\\d+)$")
     public void i_request_the_day_on(@Format("MM/dd/yyyy") Calendar calendar) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        this.calendar = calendar;
+        day = DayOfWeek.day(calendar);
     }
 
     @Then("^the day should be (.*)$")
     public void the_day_should_be(String day) throws Throwable {
-        assertThat(DayOfWeek.day(calendar), equalTo(day));
+        assertThat(this.day, equalTo(day));
     }
 }
