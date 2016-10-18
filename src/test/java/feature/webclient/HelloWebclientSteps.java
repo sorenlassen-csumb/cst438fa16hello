4489110a772befd1e793ab90e59f28ba1e253454
Soren Lassen <slassen@csumb.edu>
2016-10-17 22:13:58 -0700

    Switch HelloWebClientIT from ChromeDriver to PhantomJSDriver.
    Test plan: mvn verify

--- src/test/java/feature/webclient/HelloWebclientSteps.java
+++ src/test/java/feature/webclient/HelloWebclientSteps.java
@@ -12,12 +12,12 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelloWebclientSteps {
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new PhantomJSDriver();
 
     @After
     public void tearDown() {
